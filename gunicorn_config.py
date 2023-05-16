9f3e9ecfd9e76d64333fbedd715466f43ad80f76
Facundo Martínez <90737264+fx-biocoder@users.noreply.github.com>
2023-05-16 11:22:46 -0300

    Archivos del virtual environment
    Necesito subir estos archivos para setear el virtual environment en PythonAnywhere

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,2 +1,9 @@
 bind = "0.0.0.0:8080"
 workers = 2
+
+import sys
+path = 'C:\Users\marti\Documents\GitHub\classplanner'
+if path not in sys.path:
+   sys.path.insert(0, path)
+
+from app import app as application
